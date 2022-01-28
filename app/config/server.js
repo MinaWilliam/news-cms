94a60110438c5142ebcc8f91f277b5ac895a5aee
Mina William <mwf@flexistax.com>
2022-01-28 14:25:52 +0200

    Revert "using strapi docker image"
    This reverts commit b0834195d7eb29eccdec35153257f52f1198365a.

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '4e344acb9521db675c7c283123b57b45'),
-    },
-  },
-});
--- app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '3091536b-6574-4485-a84d-da890afe6987'
-};
