3010c5c7b3d8fffa5f7fa87ddf0f8c4e5b485640
Mina <mwfayez@gmail.com>
2022-02-02 16:48:20 +0200

    increment viewsCount on viewing an article

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -4,6 +4,22 @@
  *  article controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const {createCoreController} = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({strapi}) => ({
+    async findOne(ctx) {
+      const response = await super.findOne(ctx);
+
+      if (response.data.id && response.data.attributes.viewsCount) {
+        strapi.db.query('api::article.article').update({
+          where: {id: response.data.id},
+          data: {
+            viewsCount: parseInt(response.data.attributes.viewsCount) + 1,
+          },
+        });
+      }
+
+      return response;
+    }
+  })
+);
