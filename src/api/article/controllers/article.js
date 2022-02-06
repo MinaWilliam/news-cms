e43a2d32310b8734ee34ec34a221f39ee2f11eb7
Mina <mwfayez@gmail.com>
2022-02-06 19:51:40 +0200

    set the default article query parameters

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -6,20 +6,44 @@
 
 const {createCoreController} = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article', ({strapi}) => ({
-    async findOne(ctx) {
-      const response = await super.findOne(ctx);
+const uid = 'api::article.article';
 
-      if (response.data.id && response.data.attributes.viewsCount) {
-        strapi.db.query('api::article.article').update({
-          where: {id: response.data.id},
+module.exports = createCoreController(uid, ({strapi}) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const { query } = ctx;
+
+      query.populate = query.populate ?? {cover: {fields: ['url']}};
+
+      const entity = await strapi.service(uid).findOne(id, query);
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      if (entity.id && entity.viewsCount) {
+        strapi.db.query(uid).update({
+          where: {id: id},
           data: {
-            viewsCount: parseInt(response.data.attributes.viewsCount) + 1,
+            viewsCount: parseInt(entity.viewsCount) + 1,
           },
         });
       }
 
-      return response;
-    }
+      return this.transformResponse(sanitizedEntity);
+    },
+
+    async find(ctx) {
+      const { query } = ctx;
+
+      query.sort = query.sort ?? 'viewsCount:desc';
+      query.fields = query.fields ?? 'title,publishedAt';
+      query.populate = query.populate ?? {cover: {fields: ['url']}};
+      if (typeof query.featured !== 'undefined') {
+        query.filters =  {'featured': {'$eq': query.featured}};
+      }
+
+      const { results, pagination } = await strapi.service(uid).find(query);
+      const sanitizedResults = await this.sanitizeOutput(results, ctx);
+
+      return this.transformResponse(sanitizedResults, { pagination });
+    },
   })
 );
